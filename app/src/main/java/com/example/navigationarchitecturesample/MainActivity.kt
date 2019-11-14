f94f4cf06b5fd08e0d2533c9e3e675c6bbdca43d
yegemberdin <yegemberdin@gmail.com>
2019-11-14 12:27:13 +0600

    fragment state saving logic added

--- app/src/main/java/com/example/navigationarchitecturesample/MainActivity.kt
+++ app/src/main/java/com/example/navigationarchitecturesample/MainActivity.kt
@@ -2,40 +2,72 @@ package com.example.navigationarchitecturesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var navController: NavController
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-
-        setupBottomNavMenu(navController)
-
-        navController.addOnDestinationChangedListener{ _, destination, _ ->
-            if (destination.id == R.id.destination_login) {
-                bottom_nav?.visibility = View.GONE
-            }
-            else bottom_nav?.visibility = View.VISIBLE
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
         }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        val navGraphIds = listOf(R.navigation.home, R.navigation.profile)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
+        )
+
+        // Whenever the selected controller changes, setup the action bar.
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+            navController.addOnDestinationChangedListener{ _, destination, _ ->
+                Log.d("nazeka", resources.getResourceName(destination.id))
+                if (destination.id == R.id.destination_login) {
+                    bottom_nav?.visibility = View.GONE
+                }
+                else {
+                    bottom_nav?.visibility = View.VISIBLE
+                }
+            }        })
+        currentNavController = controller
 
     }
 
-    private fun setupBottomNavMenu(navController: NavController) {
-        bottom_nav?.let {
-            NavigationUI.setupWithNavController(it, navController)
-        }
-    }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() ?: false
+        return currentNavController?.value?.navigateUp() ?: false
     }
+
 }
--- app/src/main/java/com/example/navigationarchitecturesample/ProfileFragment.kt
+++ app/src/main/java/com/example/navigationarchitecturesample/ProfileFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
@@ -15,6 +17,13 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        details?.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_profileFragment_to_profileDetailsFragment)
+        }
     }
 
 }
